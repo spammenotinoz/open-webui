266cfeb64444ea43ce094daf4449b7e1b92f13ad
spammenotinoz <spammenotinoz@gmail.com>
2024-05-13 12:03:52 +1000

    Revert "1"
    This reverts commit 3edc1c973816aa5ea2e8bfae09c31904e6110ca1.

--- src/routes/(app)/c/[id]/+page.svelte
+++ src/routes/(app)/c/[id]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { v4 as uuidv4 } from 'uuid';
-	import toast from 'svelte-sonner';
+	import toast from 'svelte-french-toast';
 
 	import { onMount, tick } from 'svelte';
 	import { goto } from '$app/navigation';

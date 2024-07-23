09aeb8145ac013077bd984e3cf90c876cab8a24e
spammenotinoz <spammenotinoz@gmail.com>
2024-07-23 20:53:41 +1000

    Update +page.svelte

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -8,6 +8,7 @@
 	import { toast } from 'svelte-sonner';
 	import { page } from '$app/stores';
 	import { v4 as uuidv4 } from 'uuid';
+	import { userSignIn } from '$lib/apis/auths'; // Import the existing userSignIn function
 
 	const i18n = getContext('i18n');
 	const supabase = createClient('https://anrakdaroezxddxvdpaw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFucmFrZGFyb2V6eGRkeHZkcGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5NjIzNTEsImV4cCI6MjAyMzUzODM1MX0.zLZm6AI7gfZlzkseKNQNC6Ek_eDhruR6gnzl1Otk1F8');
@@ -31,34 +32,33 @@
 	};
 
 	const mapUserToDatabase = async (email) => {
-		try {
-			// Check if user exists in your application's database
-			const response = await fetch(`${WEBUI_API_BASE_URL}/check-user`, {
+		const randomPassword = uuidv4();
+		return await userSignIn(email, randomPassword).catch((error) => {
+			// If the user does not exist, create them
+			return fetch(`${WEBUI_API_BASE_URL}/auths/signup`, {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify({ email })
-			});
-			const { exists, user } = await response.json();
-
-			if (!exists) {
-				const randomPassword = uuidv4();
-				await fetch(`${WEBUI_API_BASE_URL}/create-user`, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({ email, password: randomPassword })
+				credentials: 'include',
+				body: JSON.stringify({
+					name: email.split('@')[0], // Assuming name is part of the email
+					email: email,
+					password: randomPassword,
+					profile_image_url: '' // Default or empty profile image URL
+				})
+			})
+				.then(async (res) => {
+					if (!res.ok) throw await res.json();
+					return res.json();
+				})
+				.then(() => userSignIn(email, randomPassword))
+				.catch((err) => {
+					console.log(err);
+					toast.error(err.detail);
+					return null;
 				});
-				return await mapUserToDatabase(email);  // Retry mapping to get the created user
-			}
-			return user;
-		} catch (error) {
-			console.error(error);
-			toast.error($i18n.t('An error occurred while mapping the user.'));
-			return null;
-		}
+		});
 	};
 
 	const signInHandler = async () => {

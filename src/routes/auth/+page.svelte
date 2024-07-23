6fa75fc3b32b686f5c8573814e8a0ca4fbb9e32c
spammenotinoz <spammenotinoz@gmail.com>
2024-07-23 18:37:16 +1000

    Update +page.svelte

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,143 +1,160 @@
-<script>
-    import { goto } from '$app/navigation';
-    import Spinner from '$lib/components/common/Spinner.svelte';
-    import { WEBUI_NAME, config, user, socket } from '$lib/stores';
-    import { onMount } from 'svelte';
-    import { toast } from 'svelte-sonner';
-    import { createClient } from '@supabase/supabase-js';
 
-    const supabaseUrl = 'https://anrakdaroezxddxvdpaw.supabase.co';
-    const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFucmFrZGFyb2V6eGRkeHZkcGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5NjIzNTEsImV4cCI6MjAyMzUzODM1MX0.zLZm6AI7gfZlzkseKNQNC6Ek_eDhruR6gnzl1Otk1F8'; 
+    <script>
+	import { goto } from '$app/navigation';
+	import { createClient } from '@supabase/supabase-js';
+	import Spinner from '$lib/components/common/Spinner.svelte';
+	import { WEBUI_API_BASE_URL, WEBUI_BASE_URL } from '$lib/constants';
+	import { WEBUI_NAME, config, user, socket } from '$lib/stores';
+	import { onMount, getContext } from 'svelte';
+	import { toast } from 'svelte-sonner';
+	import { page } from '$app/stores';
 
-    const supabase = createClient(supabaseUrl, supabaseAnonKey);
+	const i18n = getContext('i18n');
+	const supabase = createClient('https://anrakdaroezxddxvdpaw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFucmFrZGFyb2V6eGRkeHZkcGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5NjIzNTEsImV4cCI6MjAyMzUzODM1MX0.zLZm6AI7gfZlzkseKNQNC6Ek_eDhruR6gnzl1Otk1F8');
 
-    let loaded = false;
-    let mode = 'signin';  
-    let email = '';
-    let password = '';
+	let loaded = false;
+	let email = '';
+	let password = '';
 
-    const setSessionUser = async (session) => {
-        if (session) {
-            console.log(session);
-            toast.success('You\'re now logged in.');
-            if (session.access_token) {
-                localStorage.token = session.access_token;
-            }
+	const setSessionUser = async (sessionUser) => {
+		if (sessionUser) {
+			console.log(sessionUser);
+			toast.success($i18n.t(`You're now logged in.`));
+			if (sessionUser.access_token) {
+				localStorage.token = sessionUser.access_token;
+			}
 
-            socket.emit('user-join', { auth: { token: session.access_token } });
-            await user.set(session.user);
-            goto('/');
-        }
-    };
+			$socket.emit('user-join', { auth: { token: sessionUser.access_token } });
+			await user.set(sessionUser.user);
+			goto('/');
+		}
+	};
 
-    const signInHandler = async () => {
-        try {
-            const { data, error } = await supabase.auth.signInWithPassword({
-                email,
-                password
-            });
-            if (error) {
-                toast.error(error.message);
-                return;
-            }
-            await setSessionUser(data.session);
-        } catch (error) {
-            toast.error(error.message);
-        }
-    };
+	const signInHandler = async () => {
+		const { data, error } = await supabase.auth.signInWithPassword({
+			email,
+			password
+		});
+		if (error) {
+			toast.error(error.message);
+		} else {
+			await setSessionUser(data.session);
+		}
+	};
 
-    const submitHandler = async () => {
-        if (mode === 'signin') {
-            await signInHandler();
-        }
-    };
+	const submitHandler = async () => {
+		await signInHandler();
+	};
 
-    onMount(async () => {
-        // Check if the user is already logged in
-        const { data: { session }, error } = await supabase.auth.getSession();
-        if (session) {
-            await setSessionUser(session);
-        }
-        loaded = true; // Ensure the loading state is updated
-    });
+	onMount(async () => {
+		if ($user !== undefined) {
+			await goto('/');
+		}
+		loaded = true;
+		if (($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false) {
+			await signInHandler();
+		}
+	});
 </script>
 
 <svelte:head>
-    <title>{`${WEBUI_NAME}`}</title>
+	<title>{`${$WEBUI_NAME}`}</title>
 </svelte:head>
 
 {#if loaded}
-    <div class="fixed m-10 z-50">
-        <div class="flex space-x-2">
-            <div class="self-center">
-                <img
-                    crossorigin="anonymous"
-                    src="{WEBUI_BASE_URL}/static/favicon.png"
-                    class="w-8 rounded-full"
-                    alt="logo"
-                />
-            </div>
-        </div>
-    </div>
+	<div class="fixed m-10 z-50">
+		<div class="flex space-x-2">
+			<div class=" self-center">
+				<img
+					crossorigin="anonymous"
+					src="{WEBUI_BASE_URL}/static/favicon.png"
+					class=" w-8 rounded-full"
+					alt="logo"
+				/>
+			</div>
+		</div>
+	</div>
 
-    <div class="bg-white dark:bg-gray-950 min-h-screen w-full flex justify-center font-primary">
-        <div class="w-full sm:max-w-md px-10 min-h-screen flex flex-col text-center">
-            <div class="my-auto pb-10 w-full dark:text-gray-100">
-                <form
-                    class="flex flex-col justify-center"
-                    on:submit|preventDefault={() => {
-                        submitHandler();
-                    }}
-                >
-                    <div class="mb-1">
-                        <div class="text-2xl font-medium">
-                            {mode === 'signin' ? 'Sign in' : 'Create Account'} 
-                            to {WEBUI_NAME}
-                        </div>
-                    </div>
-                    <div class="flex flex-col mt-4">
-                        <div class="mb-2">
-                            <div class="text-sm font-medium text-left mb-1">Email</div>
-                            <input
-                                bind:value={email}
-                                type="email"
-                                class="px-5 py-3 rounded-2xl w-full text-sm outline-none border dark:border-none dark:bg-gray-900"
-                                autocomplete="email"
-                                placeholder="Enter Your Email"
-                                required
-                            />
-                        </div>
-                        <div>
-                            <div class="text-sm font-medium text-left mb-1">Password</div>
-                            <input
-                                bind:value={password}
-                                type="password"
-                                class="px-5 py-3 rounded-2xl w-full text-sm outline-none border dark:border-none dark:bg-gray-900"
-                                placeholder="Enter Your Password"
-                                autocomplete="current-password"
-                                required
-                            />
-                        </div>
-                    </div>
+	<div class=" bg-white dark:bg-gray-950 min-h-screen w-full flex justify-center font-primary">
+		<div class="w-full sm:max-w-md px-10 min-h-screen flex flex-col text-center">
+			{#if ($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false}
+				<div class=" my-auto pb-10 w-full">
+					<div
+						class="flex items-center justify-center gap-3 text-xl sm:text-2xl text-center font-semibold dark:text-gray-200"
+					>
+						<div>
+							{$i18n.t('Signing in')}
+							{$i18n.t('to')}
+							{$WEBUI_NAME}
+						</div>
 
-                    <div class="mt-5">
-                        <button
-                            class="bg-gray-900 hover:bg-gray-800 w-full rounded-2xl text-white font-medium text-sm py-3 transition"
-                            type="submit"
-                        >
-                            Sign in
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+						<div>
+							<Spinner />
+						</div>
+					</div>
+				</div>
+			{:else}
+				<div class="  my-auto pb-10 w-full dark:text-gray-100">
+					<form
+						class=" flex flex-col justify-center"
+						on:submit|preventDefault={() => {
+							submitHandler();
+						}}
+					>
+						<div class="mb-1">
+							<div class=" text-2xl font-medium">
+								{$i18n.t('Sign in')}
+								{$i18n.t('to')}
+								{$WEBUI_NAME}
+							</div>
+						</div>
+
+						<div class="flex flex-col mt-4">
+							<div class="mb-2">
+								<div class=" text-sm font-medium text-left mb-1">{$i18n.t('Email')}</div>
+								<input
+									bind:value={email}
+									type="email"
+									class=" px-5 py-3 rounded-2xl w-full text-sm outline-none border dark:border-none dark:bg-gray-900"
+									autocomplete="email"
+									placeholder={$i18n.t('Enter Your Email')}
+									required
+								/>
+							</div>
+
+							<div>
+								<div class=" text-sm font-medium text-left mb-1">{$i18n.t('Password')}</div>
+
+								<input
+									bind:value={password}
+									type="password"
+									class=" px-5 py-3 rounded-2xl w-full text-sm outline-none border dark:border-none dark:bg-gray-900"
+									placeholder={$i18n.t('Enter Your Password')}
+									autocomplete="current-password"
+									required
+								/>
+							</div>
+						</div>
+
+						<div class="mt-5">
+							<button
+								class=" bg-gray-900 hover:bg-gray-800 w-full rounded-2xl text-white font-medium text-sm py-3 transition"
+								type="submit"
+							>
+								{$i18n.t('Sign in')}
+							</button>
+						</div>
+					</form>
+				</div>
+			{/if}
+		</div>
+	</div>
 {/if}
 
 <style>
-    .font-mona {
-        font-family: 'Mona Sans', -apple-system, 'Inter', ui-sans-serif, system-ui, 'Segoe UI', 
-        Roboto, Ubuntu, Cantarell, 'Noto Sans', sans-serif, 'Helvetica Neue', Arial, 
-        'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-    }
+	.font-mona {
+		font-family: 'Mona Sans', -apple-system, 'Inter', ui-sans-serif, system-ui, 'Segoe UI', Roboto,
+			Ubuntu, Cantarell, 'Noto Sans', sans-serif, 'Helvetica Neue', Arial, 'Apple Color Emoji',
+			'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+	}
 </style>

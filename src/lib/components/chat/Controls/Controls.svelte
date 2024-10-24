0726ca74775cced64b62242f29c30df0e622e373
spammenotinoz <spammenotinoz@gmail.com>
2024-10-25 09:49:04 +1100

    Remove Valves

--- src/lib/components/chat/Controls/Controls.svelte
+++ src/lib/components/chat/Controls/Controls.svelte
@@ -59,12 +59,6 @@
 			<hr class="my-2 border-gray-50 dark:border-gray-700/10" />
 		{/if}
 
-		<Collapsible title={$i18n.t('Valves')} buttonClassName="w-full">
-			<div class="text-sm" slot="content">
-				<Valves />
-			</div>
-		</Collapsible>
-
 		<hr class="my-2 border-gray-50 dark:border-gray-700/10" />
 
 		<Collapsible title={$i18n.t('System Prompt')} open={true} buttonClassName="w-full">

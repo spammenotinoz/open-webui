9e8e644783705449db2d4ae029538813bbb17498
spammenotinoz <spammenotinoz@gmail.com>
2024-12-26 13:27:53 +1100

    Update Account.svelte

--- src/lib/components/chat/Settings/Account.svelte
+++ src/lib/components/chat/Settings/Account.svelte
@@ -239,6 +239,15 @@
 				</div>
 			</div>
 
+			
+		</div>
+
+
+		<hr class=" dark:border-gray-850 my-4" />	
+		
+		
+		
+		
 	<div class="flex justify-end pt-3 text-sm font-medium">
 		<button
 			class="px-3.5 py-1.5 text-sm font-medium bg-black hover:bg-gray-900 text-white dark:bg-white dark:text-black dark:hover:bg-gray-100 transition rounded-full"

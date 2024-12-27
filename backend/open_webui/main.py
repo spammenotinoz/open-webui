da571af6614ee283d86e811b83e95a99fe41625e
spammenotinoz <spammenotinoz@gmail.com>
2024-12-27 23:18:21 +1100

    x

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -1115,9 +1115,9 @@ async def oauth_callback(provider: str, request: Request, response: Response):
 @app.get("/manifest.json")
 async def get_manifest_json():
     return {
-        "name": WEBUI_NAME,
-        "short_name": WEBUI_NAME,
-        "description": "Open WebUI is an open, extensible, user-friendly interface for AI that adapts to your workflow.",
+        "name": "Ultimate AI",
+        "short_name": "Ultimate_AI",
+        "description": "Ultimate AI",
         "start_url": "/",
         "display": "standalone",
         "background_color": "#343541",
